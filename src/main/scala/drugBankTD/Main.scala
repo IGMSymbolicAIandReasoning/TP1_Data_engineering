87a0507a8e03d8ca6338ce4649db182001e26dea
Chicaa <croharejeanne@gmail.com>
2021-03-07 16:55:04 +0100

    initial commit

---
+++ src/main/scala/drugBankTD/Main.scala
@@ -0,0 +1,26 @@
+package drugBankTD
+
+import java.text.SimpleDateFormat
+import java.util
+
+
+object Main extends App {
+
+  val db = new LubmExtractor(LabelBase.INPUT_FILE, LabelBase.MALE_PERCENT, LabelBase.PEOPLE_VACCINATED_PERCENT, LabelBase.VACINES_PERCENT, LabelBase.VACCINES_NAMES, LabelBase.SUJECTS)
+
+  db.load()
+  print("creates data : ")
+  db.extender()
+  println("done.")
+
+  print("creates vaccine data : ")
+  db.extender_vaccine()
+  println("done.")
+
+  print("writes in file : ")
+  db.toFile(LabelBase.OUTPUT_FILE)
+  println("done")
+
+
+}
+
